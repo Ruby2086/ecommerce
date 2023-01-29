603a7588eeeb074f3fa2e44fe05722d121623f6e
Ruby2086 <akinboroabiodun@gmail.com>
2023-01-29 14:02:17 +0100

    all files added

---
+++ ecommerce-frontend/src/utils/Data.js
@@ -0,0 +1,35 @@
+export const services = [
+    {
+      title: "Free Shipping",
+      tagline: "From all orders over $5",
+      image: "images/service.png",
+      //image: "../images/cat1.png",
+    },
+  
+    {
+      title: "Daily Surprise Offers",
+      tagline: "Save upto 25% off",
+      image: "images/service-02.png",
+      //image: "../images/cat2.png",
+    },
+    {
+      title: "Support 24/7",
+      tagline: "Shop with an expert",
+      image: "images/service-03.png",
+      //image: "../images/cat3.png",
+    },
+    {
+      title: "Affordable Prices",
+      tagline: "Get Factory Default Price",
+      image: "images/service-04.png",
+      //image: "../images/ca4.png",
+    },
+    {
+      title: "Secure Payments",
+      tagline: "100% Protected Payment",
+      image: "../images/service-05.png",
+  
+      //image: "images/cat5.png",
+    },
+  ];
+  
